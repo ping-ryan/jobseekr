67cacd01a187f40fd7fd3d2d1247a14c6ba223ec
Ping Yu <pingyuuu@hotmail.com>
2022-07-14 12:57:33 -0400

    setup: api test structure

---
+++ script.js
@@ -0,0 +1,9 @@
+// API test
+
+// fetch('http://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=REPLACE&app_key=REPLACE&results_per_page=20&what=javascript%20developer&content-type=application/json')
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (jsonResponse) {
+//         console.log(jsonResponse);
+//     })
